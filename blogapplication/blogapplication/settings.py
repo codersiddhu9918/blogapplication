fbfc9232c6e3d07b75f3ab86a3d1fcb7753f40bd
codersiddhu <dwivedisiddharth1999@gmail.com>
2023-12-19 17:27:12 +0530

    urls changed

--- blogapplication/blogapplication/settings.py
+++ blogapplication/blogapplication/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(9&xng9tg!kax*xai8b^k^x!t0%j2@si&zsls_bj#225%@*&$4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
